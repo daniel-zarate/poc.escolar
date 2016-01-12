9e73fb09b119f7a50cebb0aa1b65936e3a421712
Daniel Zarate <arkano86@gmail.com>
2016-01-12 00:16:15 -0600

    Se inyecta el servicio de expediente

--- met-backEnd/src/main/java/mx/com/gunix/service/met/administracionexpediente/GuardaExpediente.java
+++ met-backEnd/src/main/java/mx/com/gunix/service/met/administracionexpediente/GuardaExpediente.java
@@ -3,6 +3,7 @@ package mx.com.gunix.service.met.administracionexpediente;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import mx.com.gunix.domain.met.administracionexpediente.Expediente;
@@ -11,9 +12,14 @@ import mx.com.gunix.framework.processes.domain.Variable;
 @Service("guardaExpediente")
 public class GuardaExpediente {
 
+	@Autowired
+	IAdministracionExpedienteService administracionExpedienteService;
+
 	public String execute(Expediente expediente){
 		
 		System.out.println("objeto expediente:" + expediente.getNombre());
+
+		administracionExpedienteService.guardarExpediente(expediente);
 		
 		return "OK";
 	} 
